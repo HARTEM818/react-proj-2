22ff4b7f81bedcd85f440e6fee44f4c4552676bb
HARTEM818 <artemhavryliuk777@gmail.com>
2025-11-06 23:14:21 +0200

    third

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import { Component } from "react";
-
 import "./App.css";
+import Card from "./card.tsx";
+import colors from "./colors";
 
-class App extends Component {
+type AppState = {
+  selectedColor: string;
+};
+
+class App extends Component<{}, AppState> {
+  state: AppState = {
+    selectedColor: "",
+  };
+  changing = (color: string) => {
+    this.setState({ selectedColor: color });
+  };
+
   render() {
     return (
-     <>
-      
-     </>
+      <div className="App">
+        {colors.map((el) => {
+          return (
+            <Card onClick={this.changing} isSelected={this.state.selectedColor === el} color={el} />
+          );
+        })}
+      </div>
     );
   }
 }
--- src/card.tsx
+++ src/card.tsx
@@ -2,16 +2,19 @@ import { Component } from "react";
 import "./card.css";
 type CardProps = {
   color: string;
-}
+  isSelected: boolean;
+  onClick: (color: string) => void;
+};
 
 class Card extends Component<CardProps> {
- 
   render() {
     return (
       <>
-        <div className="card">
-
-        </div>
+        <div
+          className="card"
+          onClick={() => this.props.onClick(this.props.color)}
+          style={{ backgroundColor: this.props.color, border: this.props.isSelected ? "5px solid red" : "2px solid black" }}
+        ></div>
       </>
     );
   }
