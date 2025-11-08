8556b3faa459d7581cfbc82edce1611635c9995d
HARTEM818 <artemhavryliuk777@gmail.com>
2025-11-08 20:17:07 +0200

    I hope this was last update

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Card from "./card.tsx";
 import Indicator from "./indicator.tsx";
 import colors from "./colors";
+import Title from "./title.tsx";
+
 
 type AppState = {
   selectedColor: string | null;
@@ -19,12 +21,19 @@ class App extends Component<{}, AppState> {
   render() {
     return (
       <div className="App">
-        <Indicator bgColor={this.state.selectedColor}/>
-        {colors.map((el) => {
-          return (
-            <Card onClick={this.changing} isSelected={this.state.selectedColor === el} color={el} />
-          );
-        })}
+        <Indicator bgColor={this.state.selectedColor} />
+        <Title/>
+        <div className="card-wrapper">
+          {colors.map((el) => {
+            return (
+              <Card
+                onClick={this.changing}
+                isSelected={this.state.selectedColor === el}
+                color={el}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
---
+++ src/title.tsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+
+class Title extends Component{
+  
+  render() {
+    return <h2 style={{color: "gray"}}>Кликни на квадрат і колір покажеться на таблі</h2>
+  }
+}
+export default Title;
